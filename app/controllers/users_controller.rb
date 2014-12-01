96295f4561607c72a07a1a35ab30cd88d9cccc7e
ljdegastines <lj.degastines@gmail.com>
2014-12-01 23:25:53 +0100

    updated css and basic layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,7 @@
-class UserController < ApplicationController
-  def index
-  end
+class UsersController < ApplicationController
+
 
   def show
   end
 
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 end
