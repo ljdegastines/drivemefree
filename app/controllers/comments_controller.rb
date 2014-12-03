a7c127d3e304f13fe28b78eb481fb67d0f2c1475
ljdegastines <lj.degastines@gmail.com>
2014-12-03 15:26:57 +0100

    search working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,8 @@
 class CommentsController < ApplicationController
+
+  # def destroy
+  #   @comment = Comment.find(params[:id])
+  #   @comment.destroy if @comment.owner == current_user
+  # end
+
 end
--- app/controllers/motorcycles_controller.rb
+++ app/controllers/motorcycles_controller.rb
@@ -2,11 +2,7 @@ class MotorcyclesController < ApplicationController
   before_action :get_motorcycle, only: [:show, :edit, :update, :destroy]
 
   def index
-    if params = {}
-      @motorcycles = Motorcycle.all
-    else
-      @motorcycles = Motorcycle.where(params)
-    end
+    @motorcycles = Motorcycle.all
   end
 
   def show
@@ -40,14 +36,21 @@ class MotorcyclesController < ApplicationController
     redirect_to motorcycles_path
   end
 
+  def search
+    @motorcycles = Motorcycle.where(search_params.reject{|_, v| v.blank?})
+  end
 
   private
 
   def get_motorcycle
-  @motorcycle = Motorcycle.find(params[:id])
+    @motorcycle = Motorcycle.find(params[:id])
   end
 
   def motorcycle_params
     params.require(:motorcycle).permit(:manufacturer, :model, :style, :production_year, :engine_size, :engine_type, :horsepower, :license_plate, :description, :pickup_address, :longitude, :latitude, :autonomy_km, :picture)
+  end
+
+  def search_params
+    params.permit(:manufacturer, :model, :style, :production_year, :engine_size, :engine_type, :horsepower, :license_plate, :description, :pickup_address, :longitude, :latitude, :autonomy_km, :picture)
   end
 end
