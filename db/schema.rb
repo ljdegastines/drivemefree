d023e6d7a211b12637b3aca8a880f910aa2fe302
ljdegastines <lj.degastines@gmail.com>
2014-12-04 18:48:08 +0100

    clean git

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20141204134710) do
+ActiveRecord::Schema.define(version: 20141204151004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +71,10 @@ ActiveRecord::Schema.define(version: 20141204134710) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "style"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
     t.string   "city"
     t.integer  "zip_code"
     t.integer  "price_per_day"
