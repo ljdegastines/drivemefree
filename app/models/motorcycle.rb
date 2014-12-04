db41470d8f2633a0dff214c89d08d75b74c91b2f
MaximeHerne <herne.maxime@gmail.com>
2014-12-04 12:19:46 +0100

    updating vignettes for image

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -2,12 +2,7 @@ class Motorcycle < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
   has_many :photos
 
-  MANUFACTURERS = %w(BMW Guzzi Triumph Ducati)
+  MANUFACTURERS = %w(BMW Ducati Guzzi Honda Kawasaki Suzuki Triumph Yamaha)
   STYLES = %w(Roadster Trail Supermotard Trial Custom Sport)
 
-  has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
-
-  validates_attachment_content_type :picture,
-    content_type: /\Aimage\/.*\z/
 end
