0cfbd95d3bec872ce6c755c03ea27a985422c6e8
MaximeHerne <herne.maxime@gmail.com>
2014-12-04 15:16:41 +0100

    adding mandrill local + heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,5 +37,6 @@ Rails.application.configure do
 
   # Devise email
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
 
 end
