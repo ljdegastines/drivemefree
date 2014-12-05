7341a546a3da039e7ac3eee14d9eed039e2c4cd7
ljdegastines <lj.degastines@gmail.com>
2014-12-05 16:26:17 +0100

    updating booking total price and user_id

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
 
     if user.persisted?
+      raise
       sign_in_and_redirect user, event: :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
